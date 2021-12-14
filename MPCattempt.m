a993ddd505911ecad0bbdd3d18318ec98f01f990
matthollomon <95244721+matthollomon@users.noreply.github.com>
2021-12-14 10:41:19 -0500

    Add files via upload

--- MPCattempt.m
+++ MPCattempt.m
@@ -152,7 +152,8 @@ x0 = [287 , 5 , -176 , 0 , 2 , 0];
 [Y,timeIntegrate] = forwardIntegrateControlInput(U,x0);
 
 Y_ref = Y';
-U_ref = U';
+
+U_ref = [U(:,2)'; U(:,1)'];
 
 %% Parameters
 
@@ -219,7 +220,7 @@ eY = zeros(nstates,length(T));
 aQ = 1;
 bQ = 1;
 
-for i = 1 %1:length(T)-1
+for i = 1 : length(T)-1
     %shorten prediction horizon if we are at the end of trajectory
     %lengthen prediction horizon if going fast
     %shorten if going slow
@@ -228,7 +229,7 @@ for i = 1 %1:length(T)-1
     %calculate error
     eY(:,i) = Y(:,i) - Y_ref(:,i);
 
-    %generate equality constraints
+    %generate equality constraints USE REF TRAJECTORY RATHER THAN ERROR??
     [Aeq,beq] = eq_cons(i,A,B,eY(:,i),npred_i,nstates,ninputs);
     
     %generate boundary constraints
@@ -276,30 +277,55 @@ for i = 1 %1:length(T)-1
     
     H = diag([Q,repmat(R,[1,npred_i])]);
     
-    [x,fval] = quadprog(H,fdisc,[],[],Aeq,beq,Lb,Ub)
+    [x,fval] = quadprog(H,fdisc,[],[],Aeq,beq,Lb,Ub);
     
     
     %get linearized input
-    nstates*(npred_i+1)+1
-    nstates*(npred_i+1)+ninputs
-    x(nstates*(npred_i+1)+1:nstates*(npred_i+1)+ninputs)
+%     nstates*(npred_i+1)+1
+%     nstates*(npred_i+1)+ninputs
+%     x(nstates*(npred_i+1)+1:nstates*(npred_i+1)+ninputs)
     u_mpc(:,i) = x(nstates*(npred_i+1)+1:nstates*(npred_i+1)+ninputs);
-    u_mpc(:,i)
+    %u_mpc(:,i)
     
-    %get input
-    U_ref(:,i)
+%     get input
+%     U_ref(:,i)
     U(:,i) = u_mpc(:,i) + U_ref(:,i);
-    U(:,i)
     
-    Uinput = [U(2,i)',U(1,i)'];
+    %Uinput = [U(2,i)',U(1,i)'];
     
     %simulate model
-    [yTemp , fakeTime] = forwardIntegrateControlInput(Uinput,Y(:,i));
+    %[yTemp , fakeTime] = forwardIntegrateControlInput(Uinput,Y(:,i));
+    
+    [~,ztemp]=ode45(@(t,x)bike(t,x,0,U(:,i)),[0 dt], Y(:,i));
     
     %store final state
-    Y(:,i+1) = yTemp(end,:)';
+    Y(:,i+1) = ztemp(end,:)';
    
 end
+
+%% Plotting
+figure(1)
+plot(bl(1,:),bl(2,:),'k')
+hold on
+plot(br(1,:),br(2,:),'k')
+plot(cline(1,:),cline(2,:),'--k')
+plot(Y(1,:),Y(1,:),'r')
+
+figure(2)
+plot(T(1:(length(T)-1)),U_ref(2,:),'r')
+hold on
+plot(T,u_mpc(2,:),'b')
+plot(T,U(2,:),'k')
+title('Steering Inputs')
+legend('Reference','MPC','Output')
+
+figure(3)
+plot(T(1:(length(T)-1)),U_ref(1,:),'r')
+hold on
+plot(T,u_mpc(1,:),'b')
+plot(T,U(1,:),'k')
+title('Acceleration Inputs')
+legend('Reference','MPC','Output')
 
 
 %% Functions
@@ -459,5 +485,64 @@ Ub=reshape(Ub,[usize,1]);
 Lb=[-Inf(xsize,1);Lb];
 Ub=[Inf(xsize,1);Ub];
 
+end
+
+function dzdt=bike(t,x,T,U_in)
+%constants
+Nw=2;
+f=0.01;
+Iz=2667;
+a=1.35;
+b=1.45;
+By=0.27;
+Cy=1.2;
+Dy=0.7;
+Ey=-1.6;
+Shy=0;
+Svy=0;
+m=1400;
+g=9.806;
+
+
+%generate input functions CHANGE TO REFLECT ACTUAL U SIZE 
+if length(T)<=1 || isempty(T) || size(U_in,2)==1
+    delta_f=U_in(1);
+    F_x=U_in(2);
+else
+    delta_f=interp1(T',U_in(1,:)',t,'previous');
+    F_x=interp1(T',U_in(2,:)',t,'previous');
+end
+
+%slip angle functions in degrees
+a_f=rad2deg(delta_f-atan2(x(4)+a*x(6),x(2)));
+a_r=rad2deg(-atan2((x(4)-b*x(6)),x(2)));
+
+%Nonlinear Tire Dynamics
+phi_yf=(1-Ey)*(a_f+Shy)+(Ey/By)*atan(By*(a_f+Shy));
+phi_yr=(1-Ey)*(a_r+Shy)+(Ey/By)*atan(By*(a_r+Shy));
+
+F_zf=b/(a+b)*m*g;
+F_yf=F_zf*Dy*sin(Cy*atan(By*phi_yf))+Svy;
+
+F_zr=a/(a+b)*m*g;
+F_yr=F_zr*Dy*sin(Cy*atan(By*phi_yr))+Svy;
+
+F_total=sqrt((Nw*F_x)^2+(F_yr^2));
+F_max=0.7*m*g;
+
+if F_total>F_max
+    
+    F_x=F_max/F_total*F_x;
+  
+    F_yr=F_max/F_total*F_yr;
+end
+
+%vehicle dynamics
+dzdt= [x(2)*cos(x(5))-x(4)*sin(x(5));...
+          (-f*m*g+Nw*F_x-F_yf*sin(delta_f))/m+x(4)*x(6);...
+          x(2)*sin(x(5))+x(4)*cos(x(5));...
+          (F_yf*cos(delta_f)+F_yr)/m-x(2)*x(6);...
+          x(6);...
+          (F_yf*a*cos(delta_f)-F_yr*b)/Iz];
 end
 
